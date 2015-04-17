cdf8c4f9307a522e30b2dfa47e79dbc36b88d5e7
Yuriy <ua@ifstudio.org>
2015-04-17 13:19:00 +0400

    some fixes to the init.php

--- lib/init.php
+++ lib/init.php
@@ -21,6 +21,10 @@ include "main.func.php";
 // админки и витрины функциями
 include "shared.php";
 
+// Подключение файла с функциями для работы с "Корзиной заказов"
+//include "cart.php"; ZAKHAR 4/17/2015
+include "cart/point.php";
+
 // Подключение языкового файла
 include "mad/lang/english.php";
 
@@ -50,9 +54,22 @@ if (!empty($_GET['id']))	$id = trimmer($_GET['id']);
 // Старт сессии
 session_start();
 
-if ($_SESSION && $_SESSION['loggedin'] == "yes") { error_reporting(E_ALL); }
+//if ($_SESSION && $_SESSION['loggedin'] == "yes") { error_reporting(E_ALL); }
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == "yes") {
+	error_reporting(E_ALL);
+}
 // else echo $_SERVER['REMOTE_ADDR'];
 
+if (!isset($_SESSION['cart'])) {
+	$_SESSION['cart'] = array();
+	$_SESSION['total'] = 0;
+	$_SESSION['fullCost'] = 0;
+	$_SESSION['discount'] = 0;
+	$_SESSION['quantity'] = 0;
+	$_SESSION['orderId'] = null;
+}
+
+cartClean($link);
 
 // Получение базовых параметров системы в глобальную переменную $Settings
 getSettings();
@@ -70,7 +87,8 @@ $page = PageGen($id);
 if ($page['parent_id']=="-1") $template="index";
 
 // Если пользователь залогинен, даем ему право на редактирование
-if ($_SESSION && $_SESSION['loggedin']== "yes") 
+//if ($_SESSION && $_SESSION['loggedin']== "yes") 
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == "yes")
 $adminedit="<span class='adminedit'>
 <a href='/mad/pages/edit/$page[id]?parent_id=$page[parent_id]'>edit</a> 
 <a href='/mad/pages/'>admin</a>
